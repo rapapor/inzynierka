989668ef3c33674b47fd88deb02f64d01b97fd1e
adrianwisniewski <adrian.wisniewski@empsgroup.com>
2018-11-13 22:18:08 +0100

    test api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import logo from './assets/images/logo.jpg'
 
 class App extends Component {
+
+  state = {
+    contacts: ''
+  };
+
+  componentDidMount() {
+
+    fetch('https://ffr-api.herokuapp.com/auth', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: 'admin',
+        password: 'admin',
+      }),
+    }).then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson)
+  })
+}
+
   render() {
     return (
       <div className="App">
